02bb2ce25f79cb3a9a024eefed0efb0960759a7e
Kazik Pogoda <morisil@xemantic.com>
2023-02-11 10:52:53 +0100

    initial version

---
+++ main.tf
@@ -0,0 +1,100 @@
+resource "google_project" "terraform" {
+  org_id              = var.org_id
+  billing_account     = var.billing_account
+  project_id          = var.terraform_project
+  name                = var.terraform_project_name
+  auto_create_network = false
+  skip_delete         = true
+}
+
+resource "google_project_service" "cloudbilling" {
+  project    = google_project.terraform.project_id
+  service    = "cloudbilling.googleapis.com"
+  depends_on = [google_project.terraform]
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  project    = google_project.terraform.project_id
+  service    = "cloudresourcemanager.googleapis.com"
+  depends_on = [google_project.terraform]
+}
+
+resource "google_project_service" "iam" {
+  project    = google_project.terraform.project_id
+  service    = "iam.googleapis.com"
+  depends_on = [google_project.terraform]
+}
+
+resource "google_project_service" "serviceusage" {
+  project    = google_project.terraform.project_id
+  service    = "serviceusage.googleapis.com"
+  depends_on = [google_project.terraform]
+}
+
+resource "google_project_default_service_accounts" "terraform" {
+  project = google_project.terraform.project_id
+  action  = "DELETE"
+}
+
+resource "google_service_account" "terraform" {
+  project      = google_project.terraform.project_id
+  account_id   = var.terraform_service_account_id
+  display_name = var.terraform_service_account_name
+}
+
+// google_service_account creation is eventually consistent, to prevent errors we wait a bit
+resource "time_sleep" "after_terraform_service_account" {
+  depends_on      = [google_service_account.terraform]
+  create_duration = "10s"
+}
+
+data "google_iam_policy" "terraform" {
+  binding {
+    role    = "roles/owner"
+    members = [google_service_account.terraform.member]
+  }
+}
+
+resource "google_project_iam_policy" "terraform" {
+  project     = google_project.terraform.project_id
+  policy_data = data.google_iam_policy.terraform.policy_data
+  depends_on  = [
+    time_sleep.after_terraform_service_account,
+    google_project_service.iam
+  ]
+}
+
+resource "google_storage_bucket" "terraform" {
+  project  = google_project.terraform.project_id
+  location = var.location
+  // bucket names have to be globally unique, as project ids,
+  // so the project_id is a safe bet here
+  name     = google_project.terraform.project_id
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_service_account_iam_binding" "terraform_impersonation" {
+  service_account_id = google_service_account.terraform.id
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = var.terraformers
+}
+
+resource "google_organization_iam_member" "terraform_project_creator" {
+  org_id = var.org_id
+  role   = "roles/resourcemanager.projectCreator"
+  member = google_service_account.terraform.member
+}
+
+resource "google_organization_iam_member" "terraform_project_iam_admin" {
+  org_id = var.org_id
+  role   = "roles/resourcemanager.projectIamAdmin"
+  member = google_service_account.terraform.member
+}
+
+resource "google_organization_iam_member" "terraform_billing_user" {
+  org_id = var.org_id
+  role   = "roles/billing.user"
+  member = google_service_account.terraform.member
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "org_id" {
+  description = "The Google Cloud organization id"
+  type        = string
+}
+
+variable "billing_account" {
+  description = "The Billing Account to associate Terraform project with"
+  type        = string
+}
+
+variable "terraform_project_id" {
+  description = "The id of Google Cloud Project administering Terraform (must be globally unique)"
+  type        = string
+}
+
+variable "terraform_project_name" {
+  description = "The name of Google Cloud Project administering Terraform"
+  type        = string
+  default     = "Terraform Admin Project"
+}
+
+variable "terraform_service_account_id" {
+  description = "The id of service account managing this project"
+  type        = string
+  default     = "terraform"
+}
+
+variable "terraform_service_account_name" {
+  description = "The id of service account managing this project"
+  type        = string
+  default     = "Terraform Service Account"
+}
+
+variable "location" {
+  description = "The location where terraform state bucket is created"
+  type        = string
+}
+
+variable "terraformers" {
+  description = "The list of members who can impersonate terraform service account"
+  type        = list(string)
+}
